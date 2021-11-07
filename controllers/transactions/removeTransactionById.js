08ef9fed8bff3e9b580bf7efcd92bfdf5a178a1e
Stas Kotelnykov <staskotelnykov@gmail.com>
2021-11-07 09:39:03 +0200

    Update transaction deleting

--- controllers/transactions/removeTransactionById.js
+++ controllers/transactions/removeTransactionById.js
@@ -2,14 +2,14 @@ const { sendSuccessRes } = require('../../helpers')
 const { Transaction } = require('../../models')
 
 const removeTransactionById = async (req, res) => {
-  const id = req.params.contactId
+  const id = req.params.transactionId
   const result = await Transaction.findByIdAndDelete(id)
 
   if (!result) {
     res.status(404).json({
       status: 'error',
       code: 404,
-      message: `Contact with id:${req.params.contactId} not found`,
+      message: `Transaction with id:${req.params.transactionId} not found`,
     })
     return
   }
