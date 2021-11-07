2d5195c8b6acbbeb8e31f05d1bfbf75cb152c095
vladyslav <fakingbrutal@gmail.com>
2021-11-07 21:17:39 +0200

    2-commit
    transactionId поправив видалення.

--- controllers/transactions/removeTransactionById.js
+++ controllers/transactions/removeTransactionById.js
@@ -2,18 +2,17 @@ const { sendSuccessRes } = require('../../helpers')
 const { Transaction } = require('../../models')
 
 const removeTransactionById = async (req, res) => {
-  const id = req.params.contactId
+  const id = req.params.transactionId
   const result = await Transaction.findByIdAndDelete(id)
-
   if (!result) {
     res.status(404).json({
       status: 'error',
       code: 404,
-      message: `Contact with id:${req.params.contactId} not found`
+      message: `Transaction with id:${req.params.contactId} not found`
     })
     return
   }
-  sendSuccessRes(res, { message: 'Contact deleted' })
+  sendSuccessRes(res, { message: 'Transaction deleted' })
 }
 
 module.exports = removeTransactionById
