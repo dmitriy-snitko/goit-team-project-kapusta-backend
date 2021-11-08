5f05ce76693d1cf1aae2b8713801b79a8e1e7c7c
Stas Kotelnykov <staskotelnykov@gmail.com>
2021-11-08 14:31:28 +0200

    update server
    Database connection successful

--- controllers/transactions/incoming.js
+++ controllers/transactions/incoming.js
@@ -7,7 +7,13 @@ const incoming = async (req, res) => {
   const month = data.toLocaleString('ru', monthOptions)
   const yearOptions = { year: 'numeric', timezone: 'UTC' }
   const year = data.toLocaleString('ru', yearOptions)
-  const newOutgoing = { ...req.body, owner: res.locals.user._id, month: month, year: year, typeOftransactions: true }
+  const newOutgoing = {
+    ...req.body,
+    owner: res.locals.user._id,
+    month: month,
+    year: year,
+    typeOftransactions: true,
+  }
   const result = await Transaction.create(newOutgoing)
   sendSuccessRes(res, { result }, 201)
 }
