e3f206173f8507ef68932003d82aded519b25a25
vladyslav <fakingbrutal@gmail.com>
2021-11-26 11:29:53 +0200

    2-commit.

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ app.use(logger(formatsLogger))
 app.use(cors())
 app.use(express.json())
 app.use(express.json({ limit: 10000 })) // обмеження об'єму
-// app.use(boolParser()) // обов'зково після express.json
+app.use(boolParser()) // обов'зково після express.json
 
 app.use('/api/', rateLimit(limiterApi))
 app.use('/api/users', usersRouter)
