f67c561a44f9955b2e9026039a25c985aa2f2a22
Dmitriy Snitko <dmitriy.snitko@gmail.com>
2021-12-16 21:15:26 +0200

    bugfix email

--- controllers/users/signUp.js
+++ controllers/users/signUp.js
@@ -1,15 +1,19 @@
 const Users = require('../../repositories')
 const { Conflict } = require('http-errors')
 const { CREATED } = require('../../helpers/constants')
+const { createEmail, sendEmail } = require('../../helpers')
 
 const signUp = async (req, res, next) => {
   const user = await Users.findUserByEmail(req.body.email)
   res.locals.user = user
+
   if (user) {
     throw new Conflict('Email in use')
   }
 
-  await Users.createUser(req.body)
+  const { verifyToken } = await Users.createUser(req.body)
+  const data = createEmail(req, verifyToken)
+  await sendEmail(data)
 
   return res
     .status(CREATED)
--- helpers/createEmail.js
+++ helpers/createEmail.js
@@ -1,66 +1,64 @@
-const { BASE_URL } = process.env
+const createEmail = (req, verifyToken) => {
+  const { BASE_URL } = process.env
+  const { email, name } = req.body
+  const data = {
+    to: email,
+    subject: 'Please confirm your email',
+    html: `
+      <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+      <html xmlns="http://www.w3.org/1999/xhtml">
+      
+      <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        
+        <title>Document</title>
+        
+        <style>
+          @import url('https://fonts.googleapis.com/css2?family=Roboto:wght@400;900&display=swap');
+        </style>
+      </head>
+      
+      <body style="margin: 0; font-family: Arial, Helvetica, sans-serif;">
+        <table style="width: 100%; max-width: 768px; margin: 0 auto; border-collapse: collapse; background-repeat: no-repeat; font-size: 14px; color: #52555F" background="https://i.ibb.co/nMSYP6k/bgTop.jpg">
+          <tr>
+            <td></td>
+            <td style="height: 100px; width: 436px; background: url(https://i.ibb.co/q1k4MQP/logo.png); background-repeat: no-repeat; background-position: right bottom;"></td>
+            <td></td>
+          </tr>
+          <tr>
+            <td></td>
+            <td style="padding-top: 40px; text-align: center;">
+              <div style="padding: 30px; box-sizing: border-box; border-radius: 30px; border-right: 3px solid #e1e1e1; border-left: 1px solid #F5F6FB; border-bottom: 3px solid #e1e1e1; background: #FFFFFF;">
+              <p style="text-align: left; margin-bottom: 30px;">Hello, ${name}!</p>
 
-const createEmail = (name, verifyToken) => {
-  const html = `<!DOCTYPE html
-  PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
+              <p style="text-align: left; margin-bottom: 30px;">
+                You registered an account on <span>Kapusta smart finance</span>, before being able to use your account you
+                need
+                to verify that this is your email address by clicking here:
+              </p>
+              <a href="${BASE_URL}/api/users/verify/${verifyToken}"
+                style="text-transform: uppercase; background: #FF751D; padding: 12px 34px; border-radius: 16px; color: #FFFFFF; text-decoration: none; display: inline-block; margin: 0 auto; margin-bottom: 15px;">Verify
+                Your email
+              </a>
 
-<head>
-  <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
-  <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-  <title>Document</title>
-  <style>
-    @import url('https://fonts.googleapis.com/css2?family=Roboto:wght@400;900&display=swap');
+              <p style="text-align: left; margin-bottom: 30px;">
+                Kind Regards, <span>Kapusta smart finance.</span>
+              </p>
+              <div style="height: 150px; background: url('https://i.ibb.co/Vv7ssKj/bgBottom.jpg'); background-repeat: no-repeat; background-position: bottom right;"></div>
+            </div>
+            </td>
+          <td></td>
+        </tr>
+      </table>
 
-  </style>
-</head>
+      </div>
+    </body>
 
-<body style="margin: 0; font-family: Arial, Helvetica, sans-serif;">
-  <table style="width: 100%; max-width: 768px; margin: 0 auto; border-collapse: collapse; background-repeat: no-repeat; font-size: 14px; color: #52555F"
-    background="https://i.ibb.co/nMSYP6k/bgTop.jpg">
-    <tr>
-      <td></td>
-      <td
-        style="height: 100px; width: 436px; background: url(https://i.ibb.co/q1k4MQP/logo.png); background-repeat: no-repeat; background-position: right bottom;">
-      </td>
-      <td></td>
-    </tr>
-    <tr>
-      <td></td>
-      <td style="padding-top: 40px; text-align: center;">
-        <div
-          style="padding: 30px; box-sizing: border-box; border-radius: 30px; border-right: 3px solid #e1e1e1; border-left: 1px solid #F5F6FB; border-bottom: 3px solid #e1e1e1; background: #FFFFFF;">
-          <p style="text-align: left; margin-bottom: 30px;">Hello, ${name}!</p>
-
-          <p style="text-align: left; margin-bottom: 30px;">
-            You registered an account on <span>Kapusta smart finance</span>, before being able to use your account you
-            need
-            to verify that this is your email address by clicking here:
-          </p>
-         <a href="${BASE_URL}/api/users/verify/${verifyToken}"
-            style="text-transform: uppercase; background: #FF751D; padding: 12px 34px; border-radius: 16px; color: #FFFFFF; text-decoration: none; display: inline-block; margin: 0 auto; margin-bottom: 15px;">Verify
-            Your email
-          </a>
-
-          <p style="text-align: left; margin-bottom: 30px;">
-            Kind Regards, <span>Kapusta smart finance.</span>
-          </p>
-          <div style="height: 150px; background: url('https://i.ibb.co/Vv7ssKj/bgBottom.jpg'); background-repeat: no-repeat; background-position: bottom right;">
-          </div>
-        </div>
-      </td>
-      <td></td>
-    </tr>
-  </table>
-
-  </div>
-</body>
-
-</html>
-  `
-
-  return html
+    </html>`
+  }
+  return data
 }
 
 module.exports = createEmail
