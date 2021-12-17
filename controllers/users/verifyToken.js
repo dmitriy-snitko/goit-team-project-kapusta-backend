6ebe26deca1db9ec89f749488ac96ecd8e0db065
Dmitriy Snitko <dmitriy.snitko@gmail.com>
2021-12-17 02:50:32 +0200

    Update verifyToken.js

--- controllers/users/verifyToken.js
+++ controllers/users/verifyToken.js
@@ -3,15 +3,12 @@ const { User } = require('../../models')
 
 const verifyToken = async (req, res) => {
   const { verifyToken } = req.params
-  const user = await User.findOne({ verifyToken })
+  const user = await User.findOneAndUpdate({ verifyToken }, { verify: true })
 
   if (!user) {
     throw new NotFound('User not found')
   }
-  await User.findOneAndUpdate(
-    { verifyToken },
-    { verifyToken: null, verify: true }
-  )
+
   res.redirect(process.env.HOME_URL)
 }
 
